0c564a57cb540895281610f4499046d96acfb241
Eduardo Pinho <enet4mikeenet@gmail.com>
2023-11-25 16:26:46 +0000

    Fix price changing UI for good
    - add unit tests
    - improve logic for raising and lowering price based on current price

--- src/components/services.rs
+++ src/components/services.rs
@@ -115,28 +115,7 @@ impl Component for CloudService {
             let price = ctx.props().price;
             let on_lower_price = Callback::from(move |_e: MouseEvent| {
                 play_zip_click();
-                // based on current price, decide how to lower it
-                let new_price;
-                if price <= Money::millicents(1) {
-                    new_price = Money::millicents(1);
-                } else if price < Money::millicents(20) {
-                    new_price = price - Money::millicents(1);
-                } else if price < Money::millicents(100) {
-                    new_price = price - Money::millicents(5);
-                } else if price < Money::millicents(500) {
-                    new_price = price - Money::millicents(10);
-                } else if price < Money::millicents(1_000) {
-                    new_price = price - Money::millicents(100);
-                } else if price < Money::cents(10) {
-                    new_price = price - Money::cents(1);
-                } else if price < Money::cents(50) {
-                    new_price = price - Money::cents(5);
-                } else if price < Money::dollars(2) {
-                    new_price = price - Money::cents(10);
-                } else {
-                    new_price = price - Money::cents(50);
-                }
-
+                let new_price = lower_price(price);
                 on_price_change.emit(new_price);
             });
             on_lower_price
@@ -147,26 +126,7 @@ impl Component for CloudService {
             let price = ctx.props().price;
             let on_raise_price = Callback::from(move |_e: MouseEvent| {
                 play_zip_click();
-                // based on current price, decide how to raise it
-                let new_price;
-                if price >= Money::dollars(50) {
-                    new_price = price + Money::cents(50);
-                } else if price >= Money::dollars(2) {
-                    new_price = price + Money::cents(10);
-                } else if price >= Money::cents(50) {
-                    new_price = price + Money::cents(5);
-                } else if price >= Money::cents(10) {
-                    new_price = price + Money::cents(1);
-                } else if price >= Money::cents(1) {
-                    new_price = price + Money::millicents(100);
-                } else if price >= Money::millicents(100) {
-                    new_price = price + Money::millicents(10);
-                } else if price >= Money::millicents(20) {
-                    new_price = price + Money::millicents(5);
-                } else {
-                    new_price = price + Money::millicents(1);
-                }
-
+                let new_price = raise_price(price);
                 on_price_change.emit(new_price);
             });
             on_raise_price
@@ -210,5 +170,107 @@ impl Component for CloudService {
                 }
             </div>
         }
+    }
+}
+
+/// based on current price, decide how to lower it
+fn lower_price(price: Money) -> Money {
+    if price <= Money::millicents(1) {
+        Money::millicents(1)
+    } else if price <= Money::millicents(20) {
+        price - Money::millicents(1)
+    } else if price <= Money::millicents(100) {
+        price - Money::millicents(5)
+    } else if price <= Money::millicents(200) {
+        price - Money::millicents(10)
+    } else if price <= Money::cents(1) {
+        price - Money::millicents(50)
+    } else if price <= Money::cents(2) {
+        price - Money::millicents(100)
+    } else if price <= Money::cents(10) {
+        price - Money::millicents(500)
+    } else if price <= Money::cents(20) {
+        price - Money::cents(1)
+    } else if price <= Money::dollars(1) {
+        price - Money::cents(5)
+    } else if price <= Money::dollars(2) {
+        price - Money::cents(10)
+    } else {
+        price - Money::cents(50)
+    }
+}
+
+/// based on current price, decide how to raise it
+fn raise_price(price: Money) -> Money {
+    if price >= Money::dollars(2) {
+        price + Money::cents(50)
+    } else if price >= Money::dollars(1) {
+        price + Money::cents(10)
+    } else if price >= Money::cents(20) {
+        price + Money::cents(5)
+    } else if price >= Money::cents(10) {
+        price + Money::cents(1)
+    } else if price >= Money::cents(2) {
+        price + Money::dec_cents(5)
+    } else if price >= Money::cents(1) {
+        price + Money::dec_cents(1)
+    } else if price >= Money::millicents(200) {
+        price + Money::millicents(50)
+    } else if price >= Money::millicents(100) {
+        price + Money::millicents(10)
+    } else if price >= Money::millicents(20) {
+        price + Money::millicents(5)
+    } else {
+        price + Money::millicents(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{lower_price, raise_price};
+    use crate::Money;
+
+    #[test]
+    fn test_price_changes() {
+        assert_eq!(raise_price(Money::millicents(1)), Money::millicents(2));
+        assert_eq!(raise_price(Money::millicents(10)), Money::millicents(11));
+        assert_eq!(raise_price(Money::millicents(11)), Money::millicents(12));
+        assert_eq!(raise_price(Money::millicents(20)), Money::millicents(25));
+        assert_eq!(raise_price(Money::millicents(45)), Money::millicents(50));
+        assert_eq!(raise_price(Money::millicents(50)), Money::millicents(55));
+        assert_eq!(raise_price(Money::millicents(100)), Money::millicents(110));
+        assert_eq!(raise_price(Money::dec_cents(10)), Money::dec_cents(11));
+        assert_eq!(raise_price(Money::dec_cents(11)), Money::dec_cents(12));
+        assert_eq!(raise_price(Money::dec_cents(20)), Money::dec_cents(25));
+        assert_eq!(raise_price(Money::dec_cents(45)), Money::dec_cents(50));
+        assert_eq!(raise_price(Money::cents(10)), Money::cents(11));
+        assert_eq!(raise_price(Money::cents(11)), Money::cents(12));
+        assert_eq!(raise_price(Money::cents(20)), Money::cents(25));
+        assert_eq!(raise_price(Money::cents(45)), Money::cents(50));
+        assert_eq!(raise_price(Money::cents(50)), Money::cents(55));
+        assert_eq!(raise_price(Money::cents(100)), Money::cents(110));
+
+        assert_eq!(lower_price(Money::millicents(10)), Money::millicents(9));
+        assert_eq!(lower_price(Money::millicents(11)), Money::millicents(10));
+        assert_eq!(lower_price(Money::millicents(20)), Money::millicents(19));
+        assert_eq!(lower_price(Money::millicents(25)), Money::millicents(20));
+        assert_eq!(lower_price(Money::millicents(50)), Money::millicents(45));
+        assert_eq!(lower_price(Money::millicents(55)), Money::millicents(50));
+        assert_eq!(lower_price(Money::millicents(100)), Money::millicents(95));
+        assert_eq!(lower_price(Money::millicents(110)), Money::millicents(100));
+        assert_eq!(lower_price(Money::dec_cents(10)), Money::millicents(950));
+        assert_eq!(lower_price(Money::dec_cents(11)), Money::dec_cents(10));
+        assert_eq!(lower_price(Money::dec_cents(20)), Money::dec_cents(19));
+        assert_eq!(lower_price(Money::dec_cents(25)), Money::dec_cents(20));
+        assert_eq!(lower_price(Money::cents(10)), Money::dec_cents(95));
+        assert_eq!(lower_price(Money::cents(11)), Money::cents(10));
+        assert_eq!(lower_price(Money::cents(20)), Money::cents(19));
+        assert_eq!(lower_price(Money::cents(25)), Money::cents(20));
+        assert_eq!(lower_price(Money::cents(50)), Money::cents(45));
+        assert_eq!(lower_price(Money::cents(55)), Money::cents(50));
+        assert_eq!(lower_price(Money::cents(100)), Money::cents(95));
+        assert_eq!(lower_price(Money::cents(110)), Money::cents(100));
+        assert_eq!(lower_price(Money::cents(200)), Money::cents(190));
+        assert_eq!(lower_price(Money::cents(250)), Money::cents(200));
     }
 }
